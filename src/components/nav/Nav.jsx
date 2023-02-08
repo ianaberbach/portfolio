266b47ec226e42a857cb8ba35340a24d0e4edbaa
Ian <naberbach@gmail.com>
2023-02-08 18:19:36 -0500

    styled nav bar:

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { AiOutlineExperiment } from 'react-icons/ai'
+import { BiMessageSquareDetail } from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className='active'><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser /></a>
+      <a href="#experience"><BiBook /></a>
+      <a href="#portfolio"><AiOutlineExperiment /></a>
+      <a href="#contact"><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
