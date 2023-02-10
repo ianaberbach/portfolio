16cfb83065032d128c7dc9370dd6cff15a7f5f73
Ian <naberbach@gmail.com>
2023-02-10 18:25:34 -0500

    added to about component and about css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src ={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>Year Long Coding Bootcamp</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>3 Projects</small>
+            </article>
+          </div>
+
+          <p>
+            Welcome, everyone to my personal portfolio website and thanks for taking a look at it!  My name is Ian Aberbach and I am a 24 year old aspiring software developer.  I am political science major, but always have had a love for computers, tech, and music and would like to further my horizons in those areas.  Please feel free to contact me with any questions!  
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
